307e4619048ed2c5708d4e731203f86dfe93c4a8
Ganaik2004 <137753416+Ganaik2004@users.noreply.github.com>
2024-03-10 17:16:45 +0530

    uloaded the file

--- api/index.js
+++ api/index.js
@@ -4,26 +4,28 @@ import dotenv from 'dotenv'
 import userRoutes from './routes/user.route.js'
 import cookieParser from "cookie-parser"
 import listingRouter from './routes/listing.route.js'
+import path from 'path';
+
 dotenv.config()
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log("Data base is connected")
 }).catch((err)=>{
     console.log(err);
 })
+const __dirname = path.resolve();
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-// app.get('/',(req,res)=>{
-//     res.send("Hello -world");
-// })
 
-// app.use('/api/auth',);
  app.use('/api/user',userRoutes);
  app.use('/api/listing',listingRouter)
 app.listen(3000,()=>{
     console.log("Server is started at port 3000")
 })
-
+app.use(express.static(path.join(__dirname,'/client/dist')));
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'client','dist','index.html'));
+})
 //middleware
 app.use((err,req,res,next)=>{
     const statusCode = err.statusCode||500;
