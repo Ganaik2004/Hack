b8113a69a69af10741a4cc38e7f31364d75dd490
Ganaik2004 <137753416+Ganaik2004@users.noreply.github.com>
2024-03-10 20:44:39 +0530

    uploaded

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -176,7 +176,7 @@ export default function Profile() {
   return (
     <div className='p-3 max-w-6xl mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
-      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+      <form onSubmit={handleSubmit} className='flex flex-col max-w-4xl gap-4'>
         <input
           onChange={(e) => setFile(e.target.files[0])}
           type='file'
